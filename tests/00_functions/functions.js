3e11f3573ee56e187cfc1d2fb729a03c2c0401f9
Fahmeda Khanom <Touhedakhanom14@gmail.com>
2023-02-15 10:48:40 -0500

    done HW#1

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -8,18 +8,50 @@ function max(num1, num2, num3) {
     return num2;
   }
   return num3;
+}
+
+function addUnknown(num1, num2, num3, num4, num5) {
+  if (num3 === undefined){
+    num3 = 0 
+  }
+  if (num4 === undefined){
+    num4 = 0 
+  }
+  if (num5 === undefined){
+    num5 = 0 
+  }
+
+  return (num1 + num2 + num3 + num4 + num5)
+
 
 }
 
-function addUnknown() {
+function evenOrOdd(num) {
+  if (num % 2 == 0 ){
+    return "even"
+  }
+  else{
+    return "odd"
+  }
 
 }
 
-function evenOrOdd() {
-
-}
-
-function operator() {
+function operator(num1,num2,num3) {
+  if (num1 + num2 === num3){
+    return "plus"
+  }
+  if (num1 - num2 === num3){
+    return "minus"
+  }
+  if (num1 * num2 === num3){
+    return "multiply"
+  }
+  if (num1 / num2 === num3){
+    return "divide"
+  }
+  else{
+    return null
+  }
 
 }
 
