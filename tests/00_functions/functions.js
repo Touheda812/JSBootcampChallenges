88b15b6d4209342258609a72cb182af424840a5b
Fahmeda Khanom <Touhedakhanom14@gmail.com>
2023-02-14 21:50:14 -0500

    did max

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,7 +1,13 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-  console.log(hello)
+function max(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3){
+    return num1;
+  }
+  if (num2 > num1 && num2 > num3){
+    return num2;
+  }
+  return num3;
 
 }
 
