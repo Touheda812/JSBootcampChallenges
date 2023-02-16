c2f8ccb35f3f88f4fc83dc62bb303f68ac80fb4d
Fahmeda Khanom <Touhedakhanom14@gmail.com>
2023-02-15 20:37:16 -0500

    done

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,34 +1,59 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-  const doesIncludeLetter = str.includes(letter);
-  return doesIncludeLetter; 
+  // check if the str includes the letter. if true return true
+  if (str.includes(letter)){
+    return true;
+  }
+  return false;
 }
 
 function cap(str, letter) {
-  if (str.includes(letter)){
-    const index = str.indexOf(letter)
-    const num = index+1
+  const index = str.indexOf(letter)
+  if(index === -1){
+    return 'sorry not found';
+  }
+  if(index === str.length - 1){
+    return str[0].toUpperCase();
+  }
+  const nextIndex = index + 1;
+  return str[nextIndex].toUpperCase();
+//  // else return false
+//   if (str.includes(letter)){
+//     const index = str.indexOf(letter)
+//     const num = index+1
   
-    const index1 = (str[index])
-    const num1 = (str[num]).toUpperCase()
-    return num1
-  }
-  else{
-    return "sorry not found"
-  }
-
+//     const index1 = (str[index])
+//     const num1 = (str[num]).toUpperCase()
+//     return num1
+//   }
+//   else{
+//     return "sorry not found"
+//   }
 }
 
 function firstCharacter(str1, str2) {
-  const str1_upper = str1.toUpperCase()
-  const str2_upper = str2.toUpperCase() 
-  if (str1_upper.charAt(0)===str2_upper.charAt(0)){
-    return true
+  // grab the first character from each variable and new variables.
+  // const str1FirstChar = str1[0].toLowerCase();
+  // const str2FirstChar = str2[0].toLowerCase();
+
+  if (str1[0].toLowerCase()===str2[0].toLowerCase()){
+    return true;
   }
-  else{
-    return false
-  }
+  
+  // if first chars are equal return true 
+
+  // else return false
+  return false;
+
+  // const str1_upper = str1.toUpperCase()
+  // const str2_upper = str2.toUpperCase() 
+  // if (str1_upper.charAt(0)===str2_upper.charAt(0)){
+  //   return true
+  // }
+  // else{
+  //   return false
+  // }
 }
 
 module.exports = {
--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+  let factorial = 1;
+  // for loop 
+  for (let i = 1; i<= num; i++){
+    factorial *= i 
+  }
+  // return 
+  return factorial;
 }
 
 function stringReverse(str) {
-
+  // for loop  | to count from back the index is -1 [i--]decrementing backwards
+  backwardsStr = ''
+  for (let i = str.length -1; i>=0; i--){
+    backwardsStr += str[i]
+  }
+  // return 
+  return(backwardsStr);
 }
 
 // do not use built in slice method.
 function slicer(originalString, startIdx, endIdx) {
+ 
 
 }
 
